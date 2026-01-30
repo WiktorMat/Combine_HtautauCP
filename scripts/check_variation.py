4f13321468f1000b18e441215fa8782676db7762
lucasrussell01 <lucascharlesrussell@gmail.com>
2026-01-30 12:02:21 +0000

    add labels

--- scripts/check_variation.py
+++ scripts/check_variation.py
@@ -4,7 +4,7 @@ import os
 ROOT.gStyle.SetOptStat(0)
 
 # directory containing variations
-path_to_files = '/vols/cms/lcr119/offline/HiggsCP/CMSSW_14_1_0_pre4/src/CombineHarvester/Combine_HtautauCP/outputs/Jan5_PreApp/cmb/common'
+path_to_files = '/vols/cms/lcr119/offline/HiggsCP/CMSSW_14_1_0_pre4/src/CombineHarvester/Combine_HtautauCP/outputs/Jan26_Baseline/cmb/common'
 
 # Create canvas
 c = ROOT.TCanvas("c", "c", 800, 800)
@@ -20,13 +20,13 @@ ratioPad.SetGridy()
 
 
 # systematics_to_check = ['CMS_pileup', 'CMS_HIG25012_scale_t_DM1PNet_2022EE', 'CMS_HIG25012_scale_t_DM10PNet_2022EE', 'CMS_scale_j_Absolute_2022EE', 'CMS_res_j_2022EE']
-systematics_to_check = ['CMS_HIG25012_Z_pt_reweighting']
+systematics_to_check = ['CMS_pileup']
 # processes_to_check = ['signal', 'ZTT']
-processes_to_check = ['ZTT']
+processes_to_check = ['ZTT'] #, 'JetFakes']
 
 for systematic_to_check in systematics_to_check:
 
-    output_pdf = f'syst_checks_Jan/check_vars_{systematic_to_check}.pdf'
+    output_pdf = f'syst_checks_Unblinding/check_vars_{systematic_to_check}.pdf'
     # Open the multipage PDF
     c.Print(output_pdf + "[")
 
@@ -48,11 +48,14 @@ for systematic_to_check in systematics_to_check:
                 for i, signal in enumerate(signals):
                     # add together all histos
                     h_nom_s = root_file.Get(f"{directory}/{signal}")
-                    h_up_s = root_file.Get(f"{directory}/{signal}_{systematic_to_check}Up")
-                    h_down_s = root_file.Get(f"{directory}/{signal}_{systematic_to_check}Down")
+                    h_up_s = root_file.Get(f"{directory}/{signal}_{systematic_to_check}Up_sym")
+                    h_down_s = root_file.Get(f"{directory}/{signal}_{systematic_to_check}Down_sym")
 
                     if not h_up_s or not h_down_s or not h_nom_s:
                         print("Warning: Missing one or more histograms, skipping this variation: " + f"{directory}/{signal}" + f" for syst: {systematic_to_check}")
+                        histo_nom = h_nom_s
+                        histo_up   = h_up_s
+                        histo_down = h_down_s
                         continue
 
                     if i == 0:
@@ -95,7 +98,58 @@ for systematic_to_check in systematics_to_check:
             # Main plot
             topPad.cd()
             histo_nom.SetTitle("")
-            histo_nom.GetXaxis().SetTitle(directory)
+
+
+            if 'tt_6' in directory:
+                ch_label = 'a_{1}^{3pr}a_{1}^{3pr}'
+            elif 'tt_11' in directory:
+                ch_label = 'a_{1}^{1pr}a_{1}^{3pr}'
+            elif 'tt_10' in directory:
+                ch_label = '#pi a_{1}^{1pr}'
+            elif 'tt_7' in directory:
+                ch_label = '#pi#rho'
+            elif 'tt_4' in directory:
+                ch_label = '#rho a_{1}^{1pr}/a_{1}^{1pr}a_{1}^{1pr}'
+            elif 'tt_9' in directory:
+                ch_label = '#pi a_{1}^{3pr}'
+            elif 'tt_8' in directory:
+                ch_label = '#pi#pi'
+            elif 'tt_3' in directory:
+                ch_label = '#rho#rho'
+            elif 'tt_5' in directory:
+                ch_label = '#rho a_{1}^{3pr}'
+            elif 'et_6' in directory:
+                ch_label = 'ea_{1}^{1pr}'
+            elif 'et_3' in directory:
+                ch_label = 'e#rho'
+            elif 'et_5' in directory:
+                ch_label = 'ea_{1}^{3pr}'
+            elif 'et_4' in directory:
+                ch_label = 'e#pi'
+            elif 'mt_6' in directory:
+                ch_label = '#mua_{1}^{1pr}'
+            elif 'mt_3' in directory:
+                ch_label = '#mu#rho'
+            elif 'mt_5' in directory:
+                ch_label = '#mua_{1}^{3pr}'
+            elif 'mt_4' in directory:
+                ch_label = '#mu#pi'
+            elif 'tt_2' in directory:
+                ch_label = '#tau_{h}#tau_{h} genuine background'
+            elif 'tt_2' in directory:
+                ch_label = '#tau_{h}#tau_{h} fake background'
+            elif 'mt_1' in directory:
+                ch_label = '#mu#tau_{h} genuine background'
+            elif 'mt_2' in directory:
+                ch_label = '#mu#tau_{h} fake background'
+            elif 'et_1' in directory:
+                ch_label = 'e#tau_{h} genuine background'
+            elif 'et_2' in directory:
+                ch_label = 'e#tau_{h} fake background'
+
+
+            histo_nom.GetXaxis().SetTitle(ch_label)
+
             histo_nom.SetLineColor(ROOT.kBlack)
             histo_up.SetLineColor(ROOT.kRed)
             histo_down.SetLineColor(ROOT.kBlue)
@@ -139,6 +193,29 @@ for systematic_to_check in systematics_to_check:
             ratio_up.GetXaxis().SetLabelSize(0.08)
             # ratio_up.SetMinimum(0.7)
             # ratio_up.SetMaximum(1.3)
+
+            # choose y axes limits
+            ymin = float("inf")
+            ymax = float("-inf")
+            for h in [ratio_up, ratio_down]:
+                for i in range(1, h.GetNbinsX() + 1):
+                    val = h.GetBinContent(i)
+                    err = h.GetBinError(i)
+                    if val == 0:
+                        continue
+                    ymin = min(ymin, val - err)
+                    ymax = max(ymax, val + err)
+
+            # Fallback in case something went wrong
+            if ymin == float("inf") or ymax == float("-inf"):
+                ymin, ymax = 0.9, 1.1
+
+            max_dev = max(abs(ymax - 1.0), abs(1.0 - ymin))
+            ymin = 1.0 - max_dev
+            ymax = 1.0 + max_dev
+            ratio_up.SetMinimum(ymin)
+            ratio_up.SetMaximum(ymax)
+
             ratio_up.Draw("E")
             ratio_down.Draw("E1 same")
 
